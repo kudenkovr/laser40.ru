dd5eecf83fb73f63ce2d983607cc385eb01a9f39
Kudenkov Roman <kudenkovr@gmail.com>
2021-01-29 06:21:27 +0300

    Edit Routes by new Engine\Route specifications

--- core/config/routes.php
+++ core/config/routes.php
@@ -1,6 +1,12 @@
 <?php
 return array(
 	array(
+		'ajax' => true,
+		'request' => ['foo'=>'bar'],
+		'rule' => '/',
+		'action' => 'web/index/order',
+	),
+	array(
 		'rule' => '/',
 		'action' => 'web/index/index'
 	),
